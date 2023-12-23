4a77075fadd4fa3b4e4014df7b7e62740029afba
joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>
2023-12-22 22:29:57 -0300

    init commit pt2

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+/*
+Hooks.once('init', async function() {// 'init' = FVTT is starting initialization
+
+});
+
+Hooks.once('setup', async () => { // 'setup' = game system is partially loaded
+});
+
+Hooks.once('ready', async function() { // 'ready' = FVTT is fully initialized and ready for operation
+
+});
+*/
