9e3332f0ee599e60509176eacfb6a786a7fa99bf
joaquinpereyra98 <24190917+joaquinpereyra98@users.noreply.github.com>
2023-12-30 21:49:59 -0300

    FEAT ActorClass & TemplateActor

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,26 @@
-/*
-Hooks.once('init', async function() {// 'init' = FVTT is starting initialization
+/* Hooks.once('setup', async () => { // 'setup' = game system is partially loaded }); */
+/* Hooks.once('ready', async function() { // 'ready' = FVTT is fully initialized and ready for operation });*/
 
+
+Hooks.once('init', () => { // 'init' = FVTT is starting initialization
+    class TrainerSheet extends game.swade.sheets.CharacterSheet{
+        get template(){
+            const base= 'systems/swade/templates/official/';
+            if(!game.user.isGM && this.actor.limited)return base + 'limited.hbs';
+            return "modules/PokeymanzTTRPG-FoundryVTT-Module/templates/sheet.hbs";
+        }
+        static get defaultOptions(){
+            return foundry.utils.mergeObject(super.defaultOptions,{
+            classes: ['pokeymanz', 'sheet', 'actor'],
+            width: 650,
+            height: 700,
+            resizable: true
+            });
+        }
+    }
+    Actors.registerSheet('pokeymanz', TrainerSheet, {
+        types: ['character', 'npc'],
+        //makeDefault: true,
+        label: 'POKEYMANZ.TrainerSheet',
+    });
 });
-
-Hooks.once('setup', async () => { // 'setup' = game system is partially loaded
-});
-
-Hooks.once('ready', async function() { // 'ready' = FVTT is fully initialized and ready for operation
-
-});
-*/
